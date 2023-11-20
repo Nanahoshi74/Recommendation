6f069ed94f7f10a4fe4afb312283fdaf7e5fb230
Nanahoshi74 <yukitakegl@gmail.com>
2023-11-20 23:33:30 +0900

    株価のdp、いい問題

---
+++ M-solutionsprod.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+//using mint = modint1000000007;
+// using mint = modint;  /*このときmint::set_mod(mod)のようにしてmodを底にする*/
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i,a,b) for(int i = a; i <= (int)(b); i++)
+#define rng(i,a,b) for(int i = a; i < (int)(b); i++)
+#define rrng(i,a,b) for(int i = a; i >= (int)(b); i--)
+#define pb push_back
+#define eb emplace_back
+#define pob pop_back
+#define si(a) (int)a.size()
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define ret(x) { cout<<(x)<<endl;}
+typedef long long ll;
+using namespace std;
+using P = pair<ll,ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+ll mul(ll a, ll b) { if (a == 0) return 0; if (LINF / a < b) return LINF; return min(LINF, a * b); }
+ll mod(ll x, ll y){return (x % y + y) % y;}
+char itoc(int x){return x + '0';}
+int ctoi(char c){return c - '0';}
+void chmin(ll& x,ll y){x = min(x,y);}
+void chmax(ll& x,ll y){x = max(x,y);}
+ll getnum(ll x, ll y, ll H, ll W) { (void) H; return (x * W + y);}
+template<typename T>
+void print(vector<T> &p){rep(i,si(p)) cout << p[i] << " "; cout << endl;}
+ll ceil(ll x , ll y){return (x+y-1)/y;}
+
+int main(){
+
+    ll n;
+    cin >> n;
+    vector<ll> a(n+1);
+    rng(i,1,n+1){
+        cin >> a[i];
+    }
+
+    vector<ll> dp(1000000);
+    dp[1] = 1000;
+    for(int i = 2; i <= n; i++){
+        dp[i] = dp[i-1];//何もしない場合の方が儲かる場合もあるからこれがそれにあたる
+        for(int j = 1; j < i; j++){
+            ll v = dp[j] / a[j];
+            ll w = a[i] * v + dp[j] % a[j];//儲け額からdp[j]をa[j]で割ったときの余りを忘れずに
+            chmax(dp[i],w);
+        }
+    }
+
+    cout << dp[n] << endl;
+
+
+
+    return 0;
+}
